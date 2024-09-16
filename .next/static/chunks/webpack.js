1f41801f7fbe55975e1cb05cf726ce98ff5e916f
nazhan <nazhansukri11@gmail.com>
2024-09-16 16:57:33 +0800

    remove locations dir

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa21fa35998f4399"; }
+/******/ 		__webpack_require__.h = function() { return "1092a7cfd256745e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
